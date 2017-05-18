9f0e302d362b10d22976f7e7e7c01d18c5869474
Cordis00 <www.daka@mail.ru>
2017-05-18 15:26:44 +0300

    The button on the menu is made. And automatic hiding when the window is expanded.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
 var search_btn = document.getElementById('btn_search');
 var search_form = document.getElementById('search_bar_form');
+var nav_btn = document.getElementById('btn_nav');
+var menu_form = document.getElementById('nav_modal_menu');
+
+// if (window.innerWidth > 442) {
+//   search_form.style.display = 'none';
+// }
+
+$(window).resize(function () {
+  var width = $('body').innerWidth()
+  if (width > 442) {
+    search_form.style.display = 'none';
+  }
+});
 
 btn_search.addEventListener('click', fun1);
+btn_nav.addEventListener('click', fun2);
 
 function fun1() {
   if(search_form.style.display == 'none') {
     search_form.style.display = 'block';
   }else {
     search_form.style.display = 'none';
+  }
+}
+function fun2() {
+  if(menu_form.style.display == 'none') {
+    menu_form.style.display = 'block';
+  }else {
+    menu_form.style.display = 'none';
   }
 }
