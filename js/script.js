20d1fb2748f30bba99aaf108b78dd8278852db37
Cordis00 <www.daka@mail.ru>
2017-05-19 21:35:26 +0300

    Automatic slide scrolling. Touch-slide the slides on the phones.

--- js/script.js
+++ js/script.js
@@ -31,3 +31,13 @@ function fun2() {
     menu_form.style.display = 'none';
   }
 }
+
+var hammer = new Hammer(document.querySelector('.carousel'));
+var $carousel = $(".carousel").carousel({"interval":0});
+hammer.get("swipe");
+hammer.on("swipeleft", function(){
+    $carousel.carousel("next");
+});
+hammer.on("swiperight", function(){
+    $carousel.carousel("prev");
+});
